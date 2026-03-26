d898f9a75a01d3a3fe7993c4f8aeddb7d4ddd5c0
Ashim Rai <ashimrai903@gmail.com>
2026-03-27 05:38:01 +0545

    adds hero section

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -10,6 +10,18 @@ export default function Welcome() {
                     <link rel="preconnect" href="https://fonts.bunny.net" />
                     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
                 </Head>
+                <div className="hero bg-base-200 min-h-screen">
+                    <div className="hero-content text-center">
+                        <div className="max-w-md">
+                            <h1 className="text-5xl font-bold">Hello there</h1>
+                            <p className="py-6">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                quasi. In deleniti eaque aut repudiandae et a id nisi.
+                            </p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
             </FrontLayout>
         </>
     );
